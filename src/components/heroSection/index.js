c30e7d42f1a8558f61167f9fbcfd0d6abe1054ee
Jaryd Knight <jaryd.knight@hotmail.com>
2025-01-02 14:48:20 -0500

    update photo extension for portfolio profile photo

--- src/components/heroSection/index.js
+++ src/components/heroSection/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import profilePic from "../../../public/images/IMG_0994.jpg";
+import profilePic from "../../../public/images/IMG_0994.JPG";
 import Contact from "../contact/index";
 import ExperienceSection from "../experience";
 
